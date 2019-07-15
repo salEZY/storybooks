266ca8e74cd0a3f66ef8100be7d010e3784ccf2d
salEZY <bobicaleks@yahoo.com>
2019-07-15 13:12:45 +0200

    add env

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
 module.exports = {
   mongoURI: "mongodb://salezy:salezy1@ds149947.mlab.com:49947/storybooks-dev",
-  googleClientID:
-    "995963770312-61qbckhhnuvbg4d7pvsov073jc79g6v9.apps.googleusercontent.com",
-  googleClientSecret: "AyOjGQs9szivarOkng4lgfLC"
+  googleClientID: process.env.GOOGLECLIENTID,
+  googleClientSecret: process.env.GOOGLECLIENTSECRET
 };
