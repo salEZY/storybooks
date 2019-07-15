b4ed60eeb55b070af2953fc76327ff67e2d754bb
AleksBobic <bobicaleks@yahoo.com>
2019-07-16 01:09:03 +0200

    passport next

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,12 @@ const passport = require("passport");
 
 router.get(
   "/google",
-  passport.authenticate("google", { scope: ["profile", "email"] }),
-  (req, res) => {}
-);
+  passport.authenticate("google", { scope: ["profile", "email"] }))
+
+router.get('/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/dashboard');
+  });
 
 module.exports = router;
