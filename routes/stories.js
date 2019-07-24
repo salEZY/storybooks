e7ab42245be171855dddc9be09b2e3219063fdf0
salEZY <bobicaleks@yahoo.com>
2019-07-24 17:07:13 +0200

    continue story model

--- routes/stories.js
+++ routes/stories.js
@@ -8,7 +8,13 @@ const User = mongoose.model('users')
 
 // Index
 router.get('/', (req, res) => {
-  res.render('stories/index')
+  Story.find({ status: 'public' })
+    .populate('user')
+    .then(stories => {
+      res.render('stories/index', {
+        stories: stories
+      })
+    })
 })
 
 // Show Add story page
