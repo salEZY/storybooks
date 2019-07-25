ca482a0d40a3668db2e2d326fb3a1706f00207d4
salEZY <bobicaleks@yahoo.com>
2019-07-25 16:49:47 +0200

    add dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../helpers/auth')
+const Story = mongoose.model('stories')
 
 router.get('/', ensureGuest, (req, res) => {
   res.render('index/welcome')
 })
 
 router.get('/dashboard', ensureAuth, (req, res) => {
-  res.render('index/dashboard')
+  Story.find({
+    user: req.user._id
+  }).then(stories => {
+    res.render('index/dashboard', {
+      stories: stories
+    })
+  })
 })
 
 router.get('/about', (req, res) => {
