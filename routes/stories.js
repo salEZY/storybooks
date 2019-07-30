73433bd9e6a0858c8926aa0e3cae0173c898d770
salEZY <bobicaleks@yahoo.com>
2019-07-30 11:08:41 +0200

    add access control

--- routes/stories.js
+++ routes/stories.js
@@ -17,7 +17,28 @@ router.get('/', (req, res) => {
       })
     })
 })
+
 // Show stories from user
+router.get('/user/:userId', (req, res) => {
+  Story.find({ user: req.params.userId, status: 'public' })
+    .populate('user')
+    .then(stories => {
+      res.render('stories/index', {
+        stories: stories
+      })
+    })
+})
+
+// Logged in user stories
+router.get('/my', ensureAuth, (req, res) => {
+  Story.find({ user: req.user.id })
+    .populate('user')
+    .then(stories => {
+      res.render('stories/index', {
+        stories: stories
+      })
+    })
+})
 
 // Single story GET
 router.get('/show/:id', (req, res) => {
@@ -27,9 +48,23 @@ router.get('/show/:id', (req, res) => {
     .populate('user')
     .populate('comments.commentUser')
     .then(story => {
-      res.render('stories/show', {
-        story: story
-      })
+      if (story.status == 'public') {
+        res.render('stories/show', {
+          story: story
+        })
+      } else {
+        if (req.user) {
+          if (req.user.id == story.user._id) {
+            res.render('stories/show', {
+              story: story
+            })
+          } else {
+            res.redirect('/stories')
+          }
+        } else {
+          res.redirect('/stories')
+        }
+      }
     })
 })
 
