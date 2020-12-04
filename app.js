90203798a753e33a242fea520b47648a5071fe2b
AleksBobic <bobicaleks@yahoo.com>
2020-12-04 11:40:24 +0100

    migration to Atlas refactor

--- app.js
+++ app.js
@@ -1,85 +1,85 @@
-require('dotenv').config()
-const express = require('express')
-const exphbs = require('express-handlebars')
-const path = require('path')
-const cookieSession = require('cookie-session')
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
-const cookieParser = require('cookie-parser')
-const mongoose = require('mongoose')
-const passport = require('passport')
+require("dotenv").config();
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const cookieSession = require("cookie-session");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
 
-require('./models/user')
-require('./models/story')
-require('./config/passport')(passport)
-const auth = require('./routes/auth')
-const index = require('./routes/index')
-const stories = require('./routes/stories')
-const { mongoURI } = require('./config/keys')
+require("./models/user");
+require("./models/story");
+require("./config/passport")(passport);
+const auth = require("./routes/auth");
+const index = require("./routes/index");
+const stories = require("./routes/stories");
+const { mongoURI } = require("./config/keys");
 const {
   truncate,
   stripTags,
   formatDate,
   select,
-  editIcon
-} = require('./helpers/hbs')
+  editIcon,
+} = require("./helpers/hbs");
 
 //Map global promises
-mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise;
 mongoose
   .connect(mongoURI, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
   })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err))
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
-const app = express()
+const app = express();
 
 // Handlebars
 app.engine(
-  'handlebars',
+  "handlebars",
   exphbs({
     helpers: {
       truncate: truncate,
       stripTags: stripTags,
       formatDate: formatDate,
       select: select,
-      editIcon: editIcon
+      editIcon: editIcon,
     },
-    defaultLayout: 'main'
+    defaultLayout: "main",
   })
-)
-app.set('view engine', 'handlebars')
+);
+app.set("view engine", "handlebars");
 
-app.use(cookieParser('tajna'))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(methodOverride('_method'))
+app.use(cookieParser("tajna"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride("_method"));
 app.use(
   cookieSession({
-    name: 'session',
-    keys: ['key1', 'key2'],
-    secret: 'tajna'
+    name: "session",
+    keys: ["key1", "key2"],
+    secret: "tajna",
   })
-)
-app.use(passport.initialize())
-app.use(passport.session())
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Set global vars
 app.use((req, res, next) => {
-  res.locals.user = req.user || null
-  next()
-})
+  res.locals.user = req.user || null;
+  next();
+});
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', index)
-app.use('/auth', auth)
-app.use('/stories', stories)
+app.use("/", index);
+app.use("/auth", auth);
+app.use("/stories", stories);
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log(`Server started at ${port}`)
-})
+  console.log(`Server started at ${port}`);
+});
